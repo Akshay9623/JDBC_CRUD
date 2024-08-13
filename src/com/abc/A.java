7f43f10d82afba6c97980653eb0399930d707cdc
Akshay9623 <akshaysupekar2020@gmail.com>
2024-08-13 18:59:20 +0530

    add hello mg

--- src/com/abc/A.java
+++ src/com/abc/A.java
@@ -3,11 +3,11 @@ package com.abc;
 public class A {
 public static void main(String[] args) {
 	System.out.println("main");
-<<<<<<< HEAD
+
 	System.out.println("Hello");
 	System.out.println("DEMO");
-=======
-	System.out.println("Hello Skills It");
->>>>>>> branch 'master' of https://github.com/Akshay9623/JDBC_CRUD.git
+
+	System.out.println("Hello Skills It ");
+
 }
 }
