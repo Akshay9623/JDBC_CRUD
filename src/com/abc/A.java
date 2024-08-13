a86c8b961649a09764647e0e2def7bb4b6663e42
Akshay9623 <akshaysupekar2020@gmail.com>
2024-08-13 19:09:21 +0530

    changes file dev1

--- src/com/abc/A.java
+++ src/com/abc/A.java
@@ -8,6 +8,8 @@ public static void main(String[] args) {
 	System.out.println("DEMO");
 
 	System.out.println("Hello Skills It ");
+	
+
 
 }
 }
---
+++ src/com/abc/B.java
@@ -0,0 +1,8 @@
+package com.abc;
+
+public class B {
+public void m1()
+{
+	System.out.println("m1---- method");
+}
+}
